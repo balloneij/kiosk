69e310931184ae6d75acc714269aea12805a9983
Josh Vogt <vogtjm@msoe.edu>
2021-03-15 15:48:41 -0500

    Updated FilterGroupModel to use a Set instead of list.

--- src/main/java/kiosk/models/FilterGroupModel.java
+++ src/main/java/kiosk/models/FilterGroupModel.java
@@ -1,14 +1,20 @@
 package kiosk.models;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Class for storing a list of careers to filter by. Used to filter which careers appear in a
  * spoke graph.
  */
 public class FilterGroupModel {
-    private List<String> careers;
+    private Set<String> careers;
+
+    public FilterGroupModel(Set<String> careers) {
+        this.setCareers(careers);
+    }
 
     public FilterGroupModel(List<String> careers) {
         this.setCareers(careers);
@@ -18,11 +24,15 @@ public class FilterGroupModel {
         this(Arrays.asList(careers));
     }
 
-    public void setCareers(List<String> careers) {
+    public void setCareers(Set<String> careers) {
         this.careers = careers;
     }
 
-    public List<String> getCareers() {
+    public void setCareers(List<String> careers) {
+        this.setCareers(new HashSet<>(careers));
+    }
+
+    public Set<String> getCareers() {
         return careers;
     }
 }
