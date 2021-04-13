7553b4c096169a6ce87d444cbc6198abd6ba877b
Josh Vogt <vogtjm@msoe.edu>
2021-04-12 19:05:10 -0500

    Fixed button initialization

--- src/main/java/kiosk/scenes/PathwayScene.java
+++ src/main/java/kiosk/scenes/PathwayScene.java
@@ -45,15 +45,12 @@ public class PathwayScene implements Scene {
     @Override
     public void init(Kiosk sketch) {
         if (!sketch.getRootSceneModel().getId().equals(this.model.getId())) {
-            this.homeButton = GraphicsUtil.initializeHomeButton();
-            this.homeButton.init(sketch);
+            this.homeButton = GraphicsUtil.initializeHomeButton(sketch);
             sketch.hookControl(this.homeButton);
             this.backButton = GraphicsUtil.initializeBackButton(sketch);
-            this.backButton.init(sketch);
             sketch.hookControl(this.backButton);
         } else {
             this.supplementaryButton = GraphicsUtil.initializeMsoeButton(sketch);
-            this.supplementaryButton.init(sketch);
             sketch.hookControl(this.supplementaryButton);
         }
 
--- src/main/java/kiosk/scenes/PromptScene.java
+++ src/main/java/kiosk/scenes/PromptScene.java
@@ -127,15 +127,12 @@ public class PromptScene implements Scene {
         }
 
         if (!sketch.getRootSceneModel().getId().equals(this.model.getId())) {
-            this.homeButton = GraphicsUtil.initializeHomeButton();
-            this.homeButton.init(sketch);
+            this.homeButton = GraphicsUtil.initializeHomeButton(sketch);
             sketch.hookControl(this.homeButton);
             this.backButton = GraphicsUtil.initializeBackButton(sketch);
-            this.backButton.init(sketch);
             sketch.hookControl(this.backButton);
         } else {
             this.supplementaryButton = GraphicsUtil.initializeMsoeButton(sketch);
-            this.supplementaryButton.init(sketch);
             sketch.hookControl(this.supplementaryButton);
         }
     }
