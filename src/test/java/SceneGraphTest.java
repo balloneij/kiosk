050b4b5b533d145b5287252b6e0af564f612105a
Isaac Ballone <balloneij@mini.local>
2021-04-29 21:28:23 -0500

    Replaced failing test with a more appropriate one

---
+++ src/test/java/SceneGraphTest.java
@@ -0,0 +1,41 @@
+import kiosk.SceneGraph;
+import kiosk.models.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SceneGraphTest {
+    @Test
+    void rootSceneIsSetOnInit() {
+        // Arrange
+        LoadedSurveyModel survey = new LoadedSurveyModel();
+        SceneModel scene1 = new PromptSceneModel();
+        SceneModel scene2 = new PromptSceneModel();
+        survey.scenes = new SceneModel[] { scene1, scene2 };
+        survey.rootSceneId = scene1.getId();
+
+        // Act
+        SceneGraph sceneGraph = new SceneGraph(survey);
+
+        // Assert
+        assertEquals(scene1.getId(), sceneGraph.getRootSceneModel().getId());
+    }
+
+    @Test
+    void rootSceneIsSetManually() {
+        // Arrange
+        LoadedSurveyModel survey = new LoadedSurveyModel();
+        SceneModel scene1 = new PromptSceneModel();
+        SceneModel scene2 = new PromptSceneModel();
+        survey.scenes = new SceneModel[] { scene1, scene2 };
+        survey.rootSceneId = scene1.getId();
+        SceneGraph sceneGraph = new SceneGraph(survey);
+
+        // Act
+        sceneGraph.setRootSceneModel(scene2);
+
+        // Assert
+        assertEquals(scene2.getId(), sceneGraph.getRootSceneModel().getId());
+    }
+}
